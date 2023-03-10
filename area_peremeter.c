539f639c48c888c03618782b6ccac05ce4c0b744
prisha1111 <122594618+prisha1111@users.noreply.github.com>
2023-03-10 14:05:33 +0530

    Add files via upload

---
+++ area_peremeter.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d;
+	printf("\n enter 2 numbers");
+	scanf("%d%d",&a,&b);
+	c=a*b;
+	d=2*(a+b);
+	printf("\n area=%d\n peremeter=%d ",c,d);
+	return 0;	
+}
